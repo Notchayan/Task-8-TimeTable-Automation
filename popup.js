52f3d23c924ec8dff5f5b8450a64be943d4081f1
Notchayan <130827975+Notchayan@users.noreply.github.com>
2023-06-22 04:01:20 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,102 +1,165 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Pingala TimeTable</title>
-</head>
-<link rel="stylesheet" href="style.css">
-<body>
-    <div class="center">
-        <header id = "head">
-            <h1>Course Time Table</h1>
-            <h4>Just go to add-drop courses then Click the Get the timetable butto </h4>
-            <button id="btn-timetable">Get the timetable</button>
-            <button id="btn-get-notification">Set Notification for all Classes</button>    
-        </header>
-        
-    <div class="grid">
-        <div class="header"></div>
-        <div class="header">Monday</div>
-        <div class="header">Tuesday</div>
-        <div class="header">Wednesday</div>
-        <div class="header">Thursday</div>
-        <div class="header">Friday</div>
-      
-        <div class="time">8:00 - 9:00</div>
-        <div id="mon_8-9"></div>
-        <div id="tue_8-9"></div>
-        <div id="wed_8-9"></div>
-        <div id="thu_8-9"></div>
-        <div id="fri_8-9"></div>
-        
-         <div class="time">9:00 - 10:00</div>
-        <div id="mon_9-10"></div>
-        <div id="tue_9-10"></div>
-        <div id="wed_9-10"></div>
-        <div id="thu_9-10"></div>
-        <div id="fri_9-10"></div>
-        
-         <div class="time">10:00 - 11:00</div>
-        <div id="mon_10-11"></div>
-        <div id="tue_10-11"></div>
-        <div id="wed_10-11"></div>
-        <div id="thu_10-11"></div>
-        <div id="fri_10-11"></div>
-        
-         <div class="time">11:00 - 12:00</div>
-         <div id="mon_11-12"></div>
-         <div id="tue_11-12"></div>
-         <div id="wed_11-12"></div>
-         <div id="thu_11-12"></div>
-         <div id="fri_11-12"></div>
-        
-         <div class="time">12:00 - 1:00</div>
-        <div id="mon_12-1"></div>
-        <div id="tue_12-1"></div>
-        <div id="wed_12-1"></div>
-        <div id="thu_12-1"></div>
-        <div id="fri_12-1"></div>
-        
-        <div class="time">1:00 - 2:00</div>
-        <div id="mon_1-2"></div>
-        <div id="tue_1-2"></div>
-        <div id="wed_1-2"></div>
-        <div id="thu_1-2"></div>
-        <div id="fri_1-2"></div>
-        
-        
-        <div class="time">2:00 - 3:00</div>
-        <div id="mon_2-3"></div>
-        <div id="tue_2-3"></div>
-        <div id="wed_2-3"></div>
-        <div id="thu_2-3"></div>
-        <div id="fri_2-3"></div>
-        
-        
-         <div class="time">3:00 - 4:00</div>
-         <div id="mon_3-4"></div>
-        <div id="tue_3-4"></div>
-        <div id="wed_3-4"></div>
-        <div id="thu_3-4"></div>
-        <div id="fri_3-4"></div>
-        
-        
-         <div class="time">4:00 - 5:00</div>
-         <div id="mon_4-5"></div>
-        <div id="tue_4-5"></div>
-        <div id="wed_4-5"></div>
-        <div id="thu_4-5"></div>
-        <div id="fri_4-5"></div>
-        
-        
-        <div class="time">5:00 - 6:00</div>
-        <div id="mon_5-6"></div>
-        <div id="tue_5-6"></div>
-        <div id="wed_5-6"></div>
-        <div id="thu_5-6"></div>
-        <div id="fri_5-6"></div>
-      </div>      
+document.getElementById("btn-timetable").addEventListener("click", function () {
+  var days = ["mon", "tue", "wed", "thu", "fri"];
 
-    </div>
-    <script src="popup.js"></script>
-</body>
-</html>
+  days.forEach(function (day) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { action: "get" + day },
+        function (response) {
+          // store the response
+          chrome.storage.local.set({ [day]: response }, function () {
+            console.log(day + " stored");
+          });
+        }
+      );
+    });
+  });
+});
+
+window.onload = function () {
+  var days = ["mon", "tue", "wed", "thu", "fri"];
+
+  days.forEach(function (day) {
+    chrome.storage.local.get(day, function (data) {
+      if (data[day]) {
+        document.getElementById(day + "_8-9").innerHTML = data[day].t8;
+        document.getElementById(day + "_9-10").innerHTML = data[day].t9;
+        document.getElementById(day + "_10-11").innerHTML = data[day].t10;
+        document.getElementById(day + "_11-12").innerHTML = data[day].t11;
+        document.getElementById(day + "_12-1").innerHTML = data[day].t12;
+        document.getElementById(day + "_1-2").innerHTML = data[day].t1;
+        document.getElementById(day + "_2-3").innerHTML = data[day].t2;
+        document.getElementById(day + "_3-4").innerHTML = data[day].t3;
+        document.getElementById(day + "_4-5").innerHTML = data[day].t4;
+        document.getElementById(day + "_5-6").innerHTML = data[day].t5;
+      }
+    });
+  });
+};
+
+document
+  .getElementById("btn-get-notification")
+  .addEventListener("click", function () {
+    var days = ["mon", "tue", "wed", "thu", "fri"];
+
+    days.forEach(function (day) {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(
+          tabs[0].id,
+          { action: "get" + day },
+          function (response) {
+            // store the response
+            chrome.storage.local.get(day, function (data) {
+              var days = ["mon", "tue", "wed", "thu", "fri"];
+              days.forEach(function (day) {
+                let df;
+                if (day == "mon") {
+                  df = 1;
+                }
+                if (day == "tue") {
+                  df = 2;
+                }
+                if (day == "wed") {
+                  df = 3;
+                }
+                if (day == "thu") {
+                  df = 4;
+                }
+                if (day == "fri") {
+                  df = 4;
+                }
+                if (data[day]) {
+                  if (data[day].t1) {
+                    let alarm_id =  "1_" + day;
+                    let x;
+                    const curr = new Date();
+                    const dayy = curr.getDay(); // 0 , 1, 2, 3, 4, 5, 6
+                    if (dayy - df < 0) {
+                      x = (dayy - df + 7) * 86400;
+                    } else {
+                      x = (dayy - df) * 86400;
+                    }
+                    curr = setSeconds(
+                      curr.getSeconds() + x + (12 - 8) * 3600 - 30 * 60
+                    );
+                    chrome.alarms.create(alarm_id, { when: curr.getTime() });
+                    document.getElementById(day + "_1-2").innerHTML =
+                      data[day].t1;
+                  }
+                  if (data[day].t2) {
+                    let alarm_id =  "2_" + day;
+                    let x;
+                    const curr = new Date();
+                    const dayy = curr.getDay(); // 0 , 1, 2, 3, 4, 5, 6
+                    if (dayy - df < 0) {
+                      x = (dayy - df + 7) * 86400;
+                    } else {
+                      x = (dayy - df) * 86400;
+                    }
+                    curr = setSeconds(
+                      curr.getSeconds() + x + (12 - 8) * 3600 - 30 * 60
+                    );
+                    chrome.alarms.create(alarm_id, { when: curr.getTime() });
+                    document.getElementById(day + "_2-3").innerHTML =
+                      data[day].t2;
+                  }
+                  if (data[day].t3) {
+                    let alarm_id = "3_" + day;
+                    let x;
+                    const curr = new Date();
+                    const dayy = curr.getDay(); // 0 , 1, 2, 3, 4, 5, 6
+                    if (dayy - df < 0) {
+                      x = (dayy - df + 7) * 86400;
+                    } else {
+                      x = (dayy - df) * 86400;
+                    }
+                    curr = setSeconds(
+                      curr.getSeconds() + x + (12 - 8) * 3600 - 30 * 60
+                    );
+                    chrome.alarms.create(alarm_id, { when: curr.getTime() });
+                    document.getElementById(day + "_3-4").innerHTML =
+                      data[day].t3;
+                  }
+                  if (data[day].t4) {
+                    let alarm_id =  "4_" + day;
+                    let x;
+                    const curr = new Date();
+                    const dayy = curr.getDay(); // 0 , 1, 2, 3, 4, 5, 6
+                    if (dayy - df < 0) {
+                      x = (dayy - df + 7) * 86400;
+                    } else {
+                      x = (dayy - df) * 86400;
+                    }
+                    curr = setSeconds(
+                      curr.getSeconds() + x + (12 - 8) * 3600 - 30 * 60
+                    );
+                    chrome.alarms.create(alarm_id, { when: curr.getTime() });
+                    document.getElementById(day + "_4-5").innerHTML =
+                      data[day].t4;
+                  }
+                  if (data[day].t5) {
+                    let alarm_id = "5_" + day;
+                    let x;
+                    const curr = new Date();
+                    const dayy = curr.getDay(); // 0 , 1, 2, 3, 4, 5, 6
+                    if (dayy - df < 0) {
+                      x = (dayy - df + 7) * 86400;
+                    } else {
+                      x = (dayy - df) * 86400;
+                    }
+                    curr = setSeconds(
+                      curr.getSeconds() + x + (12 - 8) * 3600 - 30 * 60
+                    );
+                    chrome.alarms.create(alarm_id, { when: curr.getTime() });
+                    document.getElementById(day + "_5-6").innerHTML =
+                      data[day].t5;
+                  }
+                }
+              });
+            });
+          }
+        );
+      });
+    });
+  });
