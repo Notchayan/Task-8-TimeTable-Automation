58f461b9c55650a19c5f0931fc20efc9bc6bfc98
Notchayan <130827975+Notchayan@users.noreply.github.com>
2023-06-21 23:37:31 +0530

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Pingala TimeTable</title>
+</head>
+<link rel="stylesheet" href="style.css">
+<body>
+    <div class="center">
+        <header id = "head">
+            <h1>Course Time Table</h1>
+            <h4>Just go to add-drop courses then Click the Get the timetable butto </h4>
+            <button id="btn-timetable">Get the timetable</button>
+            <button id="btn-get-notification">Set Notification for all Classes</button>    
+        </header>
+        
+    <div class="grid">
+        <div class="header"></div>
+        <div class="header">Monday</div>
+        <div class="header">Tuesday</div>
+        <div class="header">Wednesday</div>
+        <div class="header">Thursday</div>
+        <div class="header">Friday</div>
+      
+        <div class="time">8:00 - 9:00</div>
+        <div id="mon_8-9"></div>
+        <div id="tue_8-9"></div>
+        <div id="wed_8-9"></div>
+        <div id="thu_8-9"></div>
+        <div id="fri_8-9"></div>
+        
+         <div class="time">9:00 - 10:00</div>
+        <div id="mon_9-10"></div>
+        <div id="tue_9-10"></div>
+        <div id="wed_9-10"></div>
+        <div id="thu_9-10"></div>
+        <div id="fri_9-10"></div>
+        
+         <div class="time">10:00 - 11:00</div>
+        <div id="mon_10-11"></div>
+        <div id="tue_10-11"></div>
+        <div id="wed_10-11"></div>
+        <div id="thu_10-11"></div>
+        <div id="fri_10-11"></div>
+        
+         <div class="time">11:00 - 12:00</div>
+         <div id="mon_11-12"></div>
+         <div id="tue_11-12"></div>
+         <div id="wed_11-12"></div>
+         <div id="thu_11-12"></div>
+         <div id="fri_11-12"></div>
+        
+         <div class="time">12:00 - 1:00</div>
+        <div id="mon_12-1"></div>
+        <div id="tue_12-1"></div>
+        <div id="wed_12-1"></div>
+        <div id="thu_12-1"></div>
+        <div id="fri_12-1"></div>
+        
+        <div class="time">1:00 - 2:00</div>
+        <div id="mon_1-2"></div>
+        <div id="tue_1-2"></div>
+        <div id="wed_1-2"></div>
+        <div id="thu_1-2"></div>
+        <div id="fri_1-2"></div>
+        
+        
+        <div class="time">2:00 - 3:00</div>
+        <div id="mon_2-3"></div>
+        <div id="tue_2-3"></div>
+        <div id="wed_2-3"></div>
+        <div id="thu_2-3"></div>
+        <div id="fri_2-3"></div>
+        
+        
+         <div class="time">3:00 - 4:00</div>
+         <div id="mon_3-4"></div>
+        <div id="tue_3-4"></div>
+        <div id="wed_3-4"></div>
+        <div id="thu_3-4"></div>
+        <div id="fri_3-4"></div>
+        
+        
+         <div class="time">4:00 - 5:00</div>
+         <div id="mon_4-5"></div>
+        <div id="tue_4-5"></div>
+        <div id="wed_4-5"></div>
+        <div id="thu_4-5"></div>
+        <div id="fri_4-5"></div>
+        
+        
+        <div class="time">5:00 - 6:00</div>
+        <div id="mon_5-6"></div>
+        <div id="tue_5-6"></div>
+        <div id="wed_5-6"></div>
+        <div id="thu_5-6"></div>
+        <div id="fri_5-6"></div>
+      </div>      
+
+    </div>
+    <script src="popup.js"></script>
+</body>
+</html>
