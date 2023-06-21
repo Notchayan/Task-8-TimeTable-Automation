ccb8180ec6c41a09d88d3d0776d3c986c37292a5
Notchayan <130827975+Notchayan@users.noreply.github.com>
2023-06-21 23:38:06 +0530

    Create background.js

---
+++ background.js
@@ -0,0 +1,453 @@
+chrome.alarms.onAlarm.addListener((alarm) => {
+
+    if (alarm.name === "8_mon"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "alert8.html"
+        });
+    }
+    if (alarm.name === "9_mon"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "alert9.html"
+        });
+    }
+    if (alarm.name === "10_mon"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "alert10.html"
+        });
+    }
+    if (alarm.name === "11_mon"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "alert11.html"
+        });
+    }
+    if (alarm.name === "12_mon"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "alert12.html"
+        });
+    }
+    if (alarm.name === "1_tue"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "1alert1.html"
+        });
+    }
+    if (alarm.name === "2_tue"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "1alert2.html"
+        });
+    }
+    if (alarm.name === "3_tue"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "1alert3.html"
+        });
+    }
+    if (alarm.name === "4_tue"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "1alert4.html"
+        });
+    }
+    if (alarm.name === "5_tue"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "1alert5.html"
+        });
+    }
+    if (alarm.name === "8_tue"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "1alert8.html"
+        });
+    }
+    if (alarm.name === "9_tue"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "1alert9.html"
+        });
+    }
+    if (alarm.name === "10_tue"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "1alert10.html"
+        });
+    }
+    if (alarm.name === "11_tue"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "1alert11.html"
+        });
+    }
+    if (alarm.name === "12_tue"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "1alert12.html"
+        });
+    }
+    if (alarm.name === "1_wed"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "2alert1.html"
+        });
+    }
+    if (alarm.name === "2_wed"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "2alert2.html"
+        });
+    }
+    if (alarm.name === "3_wed"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "2alert3.html"
+        });
+    }
+    if (alarm.name === "4_wed"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "2alert4.html"
+        });
+    }
+    if (alarm.name === "5_wed"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "2alert5.html"
+        });
+    }
+    if (alarm.name === "8_wed"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "2alert8.html"
+        });
+    }
+    if (alarm.name === "9_wed"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "2alert9.html"
+        });
+    }
+    if (alarm.name === "10_wed"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "2alert10.html"
+        });
+    }
+    if (alarm.name === "11_wed"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "2alert11.html"
+        });
+    }
+    if (alarm.name === "12_wed"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "2alert12.html"
+        });
+    }
+    if (alarm.name === "1_thu"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "3alert1.html"
+        });
+    }
+    if (alarm.name === "2_thu"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "3alert2.html"
+        });
+    }
+    if (alarm.name === "3_thu"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "3alert3.html"
+        });
+    }
+    if (alarm.name === "4_thu"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "3alert4.html"
+        });
+    }
+    if (alarm.name === "5_thu"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "3alert5.html"
+        });
+    }
+    if (alarm.name === "8_thu"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "3alert8.html"
+        });
+    }
+    if (alarm.name === "9_thu"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "3alert9.html"
+        });
+    }
+    if (alarm.name === "10_thu"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "3alert10.html"
+        });
+    }
+    if (alarm.name === "11_thu"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "3alert11.html"
+        });
+    }
+    if (alarm.name === "12_thu"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "3alert12.html"
+        });
+    }
+    if (alarm.name === "1_fri"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "4alert1.html"
+        });
+    }
+    if (alarm.name === "2_fri"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "4alert2.html"
+        });
+    }
+    if (alarm.name === "3_fri"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "4alert3.html"
+        });
+    }
+    if (alarm.name === "4_fri"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "4alert4.html"
+        });
+    }
+    if (alarm.name === "5_fri"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "4alert5.html"
+        });
+    }
+    if (alarm.name === "8_fri"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "4alert8.html"
+        });
+    }
+    if (alarm.name === "9_fri"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "4alert9.html"
+        });
+    }
+    if (alarm.name === "10_fri"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "4alert10.html"
+        });
+    }
+    if (alarm.name === "11_fri"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "4alert11.html"
+        });
+    }
+    if (alarm.name === "12_fri"){
+        chrome.windows.create({
+            width: 350,
+            height: 250,
+            top: 200,
+            left: 400,
+            type: "popup",
+            url: "4alert12.html"
+        });
+    }
+});
